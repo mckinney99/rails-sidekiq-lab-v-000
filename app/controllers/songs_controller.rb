0ac84343a37ddbb64bfe9d2970ec43e40e650053
Eric McKinney <mckinney99@gmail.com>
2018-03-12 21:51:59 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,10 +24,8 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
-    redirect_to songs_path
+    LeadsWorker.perform_async(params[:leads].path)
+    redirect_to customers_path
   end
 
   def edit
@@ -59,4 +57,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
