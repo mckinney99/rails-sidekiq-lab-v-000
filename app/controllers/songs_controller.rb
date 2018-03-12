472eddc25b3b9c207673a6c9617f325ea476845a
Eric McKinney <mckinney99@gmail.com>
2018-03-12 21:52:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    LeadsWorker.perform_async(params[:leads].path)
+    LeadsWorker.perform_async(params[:songss].path)
     redirect_to customers_path
   end
 
